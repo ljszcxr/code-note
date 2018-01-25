88ae451619008c8b79bc87be570dfb248ea63e45
ljszcxr <35482872+ljszcxr@users.noreply.github.com>
2018-01-25 14:49:08 +0800

    string_format

---
+++ format.py
@@ -0,0 +1,12 @@
+s1 = "His name is {0}!".format("Arthur")
+print(s1)
+
+name = "Alice"
+age = 10
+s2 = "I am {1} and I am {0} years old.".format(age, name)
+print(s2)
+
+n1 = 4
+n2 = 5
+s3 = "2**10 = {0} and {1} * {2} = {3:f}".format(2**10, n1, n2, n1 * n2)
+print(s3)
